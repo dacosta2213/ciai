40adc81f1f77380637e0dc525fc3f48612250228
Santiago Ramos Morales <santiagoramos198@gmail.com>
2021-02-05 19:00:05 +0000

    Corrección por problemas de incompatibilidad

--- ciai/ciai/doctype/ciclo/ciclo.py
+++ ciai/ciai/doctype/ciclo/ciclo.py
@@ -9,8 +9,8 @@ from frappe.model.document import Document
 class Ciclo(Document):
 	def after_insert(self):
 		frappe.db.sql("UPDATE tabEstacion  SET ciclo = %s WHERE name = %s", (self.name,self.estacion) )
-        frappe.db.commit()
+		frappe.db.commit()
 
 	def validate(self):
 		frappe.db.sql("UPDATE tabEstacion  SET ciclo = %s WHERE name = %s", (self.name,self.estacion) )
-        frappe.db.commit()
+		frappe.db.commit()
